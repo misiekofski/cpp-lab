4f50ed9536fe0c770f8d0e9222c0119448c080f3
Michał Dobrzycki <dobrzycki@gmail.com>
2017-05-06 10:49:14 +0200

    Update main.cpp

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 #include "matrix.h"
-
 int main(int argc, char *argv[])
 {
-    matrix a;
-    a.print();
-    std::cout<<"\n";
+matrix a;
+//a.print();
+//std::cout<<"\n";
+matrix b(1);
+//b.print();
+//std::cout<<"\n";
+double tabelka[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+matrix d(tabelka);
 
-    matrix b(1);
-    b.print();
-    std::cout<<"\n";
+a=dodawanie(b,d);
+a.print();
 
-    double tabelka[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    matrix d(tabelka);
-    d.print();
+// d.print();
+// std::cout << "\n po transponowaniu \n";
+// d.t();
+// d.print();
 
-    std::cout << "\n po transponowaniu \n";
-    d.t();
-    d.print();
-
-    std::cout<<"\n";
-    std::cout<< "element 1,2 to :" << d.get(1,2) << std::endl;
-
-    d.put(1,2,34);
-    std::cout<< "element 1,2 po zmianie to :" << d.get(1,2);
-
-    //double **p=tabelka;
-    //matrix c(p);
-    //c.print();
-
-
-    return 0;
+// std::cout<<"\n";
+// std::cout<< "element 1,2 to :" << d.get(1,2) << std::endl;
+// d.put(1,2,34);
+// std::cout<< "element 1,2 po zmianie to :" << d.get(1,2);
+//double **p=tabelka;
+//matrix c(p);
+//c.print();
+return 0;
 }
